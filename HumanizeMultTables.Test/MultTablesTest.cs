1af59c9a74d04461981e4da8438be6e9c5e13917
profyoni <jrobinson@touro.edu>
2017-02-20 01:19:15 -0500

    c

---
+++ HumanizeMultTables.Test/MultTablesTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace HumanizeMultTables.Test
+{
+    public class MultTablesTest
+    {
+        [Test]
+        public void MultiplierRowWithHeader()
+        {
+            MultTables.MultiplierRow(1, 3, 4).Should().Contain("three").And.Contain("four").And.Contain("one");
+        }
+
+        [Test]
+        public void MultiplierRowWithoutHeader()
+        {
+            MultTables.MultiplierRow(1, 3, 4, header: false).Should().Contain("three").And.Contain("four").And.NotContain("one");
+        }
+    }
+}
--- HumanizeMultTables/Program.cs
+++ HumanizeMultTables/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace HumanizeMultTables
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            const int gridSize = 5;
+            Console.WriteLine($"{"",13}" + MultTables.MultiplierRow(1, 1, gridSize, header: false));
+            for (int i = 1; i <= gridSize; i++)
+            {
+                Console.WriteLine(new String('_', (gridSize + 1) * 13));
+                Console.WriteLine(MultTables.MultiplierRow(i, 1, gridSize));
+            }
+            Console.ReadLine();
         }
     }
 }
